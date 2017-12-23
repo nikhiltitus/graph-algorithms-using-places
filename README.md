eadf51a19a5ab86181cfff84cfce6be7d05bc9bd
nikhilgtitus <nikhilgeorge@umass.edu>
2017-12-22 20:14:32 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Synopsis
+
+This program implements two common graph algorithms on a problem that is easy to understand. ie: Places and Distance between them.
+## Usage
+
+Make sure that the placeinfo file contains all the required information about the distance between a pair of places
+
+Use it as:
+
+`python source destination`
+
+`python Kochi Thrissur`
+
+
+
+## License
+
+A short snippet describing the license (MIT, Apache, etc.)
---
+++ graph.py
@@ -0,0 +1,102 @@
+from collections import deque
+import sys
+import argparse
+
+class Place:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.neighbors = {}
+
+    def add_neighbor(self, place, distance):
+        self.neighbors[place] = distance
+
+
+class Graph:
+    def __init__(self):
+        self.place_map = {}
+        self.id = 1
+
+    def add_place(self, name):
+        self.id = self.id + 1
+        place = Place(name, self.id)
+        self.place_map[place.name] = place
+        return place
+
+    def get_place(self, name):
+        if name in self.place_map:
+            return self.place_map[name]
+        else:
+            return False
+    '''BFS Implementation'''
+    def check_path(self, source, destination):
+        visited_places = set()
+        queue = deque()
+        queue.append(source)
+        while queue:
+            place = queue.popleft()
+            visited_places.add(place)
+            for neighbor in place.neighbors:
+                if neighbor == destination:
+                    return True
+                elif neighbor not in visited_places:
+                    queue.append(neighbor)
+        return False
+    def min_distance(self,distance_map,visited_places):
+        min_distance=sys.maxsize
+        min_place=None
+        for place,distance in distance_map.items():
+            if distance<min_distance and place not in visited_places:
+                min_distance=distance
+                min_place=place
+        return min_place
+
+    def dijkstra(self,source,destination):
+        visited_places=set()
+        distance_map = {source.name:0}
+        current=source.name
+        while destination.name not in visited_places and current is not None:
+            current=self.min_distance(distance_map,visited_places)
+            current_place=self.place_map[current]
+            for place,distance in current_place.neighbors.items():
+                new_distance=distance_map[current]+int(distance)
+                if place not in distance_map or new_distance<distance_map[place.name]:
+                    distance_map[place.name]=new_distance
+            visited_places.add(current)
+        if destination.name in visited_places:
+            return distance_map[destination.name]
+        else:
+            return False
+
+
+def read_file(file_name):
+    graph = Graph()
+    with open(file_name) as f:
+        content = f.readlines()
+        for line in content:
+            source, destination, distance = line.split(',')
+            source_place = graph.get_place(source)
+            destination_place = graph.get_place(destination)
+            if not source_place:
+                source_place = graph.add_place(source)
+            if not destination_place:
+                destination_place = graph.add_place(destination)
+            source_place.add_neighbor(destination_place, distance)
+            destination_place.add_neighbor(source_place, distance)
+    return graph
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Check if path exist and get distance')
+    parser.add_argument('source', metavar='source', type=str,help='source string')
+    parser.add_argument('destination', metavar='destination', type=str, help='destination string')
+    parser.add_argument('fileLocation',nargs='?', metavar='fileLocation', type=str, help='destination string',default='placeinfo')
+    args = parser.parse_args()
+    graph = read_file(args.fileLocation)
+    source=args.source
+    destination=args.destination
+    print(graph.check_path(graph.get_place(source), graph.get_place(destination)))
+    print(graph.dijkstra(graph.get_place(source),graph.get_place(destination)))
+
+if __name__ == "__main__":
+    main()
