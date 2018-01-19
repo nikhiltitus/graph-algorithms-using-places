71a7ab29174046c530a3310822e03f6c168af48c
nikhiltitus <nikhiltitusk@gmail.com>
2018-01-19 11:48:41 +0530

    refactoring

--- graph.py
+++ graph.py
@@ -39,11 +39,11 @@ class Graph:
                 elif neighbor not in visited_places:
                     queue.append(neighbor)
         return False
-    def min_distance(self,distance_map,visited_places):
+    def min_distance(self,distance_map):
         min_distance=sys.maxsize
         min_place=None
         for place,distance in distance_map.items():
-            if distance<min_distance and place not in visited_places:
+            if distance<min_distance:
                 min_distance=distance
                 min_place=place
         return min_place
@@ -51,17 +51,20 @@ class Graph:
     def dijkstra(self,source,destination):
         visited_places=set()
         distance_map = {source.name:0}
-        current=source
+        current=source.name
+        current_distance=None
         while destination.name not in visited_places and current is not None:
-            current=self.min_distance(distance_map,visited_places)
-            current_place=self.place_map[current]
+            current_place = self.place_map[current]
             for place,distance in current_place.neighbors.items():
-                new_distance=distance_map[current]+int(distance)
-                if place not in distance_map or new_distance<distance_map[place.name]:
-                    distance_map[place.name]=new_distance
+                if place.name not in visited_places:
+                    new_distance=distance_map[current]+int(distance)
+                    distance_map[place.name]=min(distance_map.get(place.name,sys.maxsize),new_distance)
             visited_places.add(current)
+            current_distance=distance_map[current]
+            distance_map.pop(current,None)
+            current=self.min_distance(distance_map)
         if destination.name in visited_places:
-            return distance_map[destination.name]
+            return current_distance
         else:
             return False
 
@@ -93,7 +96,7 @@ def main():
     source=args.source
     destination=args.destination
     print('Does a path exist: '+str(graph.check_path(graph.get_place(source), graph.get_place(destination))))
-    print('Distance between source and destination'+str(graph.dijkstra(graph.get_place(source),graph.get_place(destination))))
+    print('Distance between source and destination: '+str(graph.dijkstra(graph.get_place(source),graph.get_place(destination))))
 
 if __name__ == "__main__":
     main()
--- graphtests.py
+++ graphtests.py
@@ -8,10 +8,13 @@ class GraphTest(unittest.TestCase):
         self.graph=read_file('placeinfo_test')
         self.source_place = self.graph.get_place('Kochi')
         self.destination_place = self.graph.get_place('Elanthoor')
+        self.disconnected_destination_place=self.graph.get_place('Boston')
     def test_bfs(self):
         self.assertTrue(self.graph.check_path(self.source_place,self.destination_place))
+        self.assertFalse(self.graph.check_path(self.source_place,self.disconnected_destination_place))
     def test_dijkstra(self):
-        self.assertEquals(self.graph.dijkstra(self.source_place,self.destination_place),125)
+        self.assertEqual(self.graph.dijkstra(self.source_place,self.destination_place),125)
+        self.assertFalse(self.graph.dijkstra(self.source_place,self.disconnected_destination_place))
 
 if __name__ == '__main__':
     unittest.main()
